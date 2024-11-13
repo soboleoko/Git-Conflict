ee2f60a12b70a82a83a37f665cab85e457f99110
Marcin Sobolewski <mlodysobol@gmail.com>
2024-11-13 09:27:24 +0100

    branch6

--- src/conflicts/c1/c1.java
+++ src/conflicts/c1/c1.java
@@ -1,6 +1,6 @@
 package conflicts.c1;
 
-public class c1 {
+public abstract class c1 {
     int a = 111;
     int aa = 113;
     String test = "test1";
--- src/conflicts/c2/c2.java
+++ src/conflicts/c2/c2.java
@@ -1,6 +1,6 @@
 package conflicts.c2;
 
-public class c2 {
+public abstract class c2 {
     int a = 121;
     int aa = 123;
     String test = "test2";
